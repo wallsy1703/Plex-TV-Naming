6654783e405c19cf91e100acf426b2a658fbef8d
Dr Walls <29837777+wallsy1703@users.noreply.github.com>
2018-09-26 15:21:10 +0100

    Add files via upload

--- TV Files not matcing Plex naming convention.ps1
+++ TV Files not matcing Plex naming convention.ps1
@@ -1,6 +1,7 @@
 cls 
 
-$pattern = "(.*)\s+S(\d{2})E(\d{2})[\- ]+(.*)" # regex to find match for 'TV Show - SxxExx - Show Title'
+$pattern1 = "(.*)\s+S(\d{2})E(\d{2})[\- ]+(.*)" # regex to find match for 'TV Show - SxxExx - Show Title'
+$pattern2 = "(.*)\s+S(\d{2})E(\d{3})[\- ]+(.*)" # regex to find match for 'TV Show - SxxExxx - Show Title'
 
 $path = "Z:\TV" # enter your root folder here, there is the option below to check subfolders
 
@@ -9,7 +10,7 @@ Write-Host "Scanning files....."
 # Get all files that match $pattern in a folder.
 # Add '-Recurse' switch to include files in subfolders.
 $search_results = Get-ChildItem -Path $path -Recurse `
-    | Where-Object { ((! $_.PSIsContainer) -and ($_.Name -notmatch $pattern)) } 
+    | Where-Object { ((! $_.PSIsContainer) -and ($_.Name -notmatch $pattern1)) } | Where-Object { ((! $_.PSIsContainer) -and ($_.Name -notmatch $pattern2)) }
 
 
 foreach ($file in $search_results) {
